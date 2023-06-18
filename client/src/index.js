16668b02a5d804c645a260b3bb913a75d843a04e
PatrickBoynton <jpboynton3@gmail.com>
2023-06-18 14:16:48 -0400

    Added react router, and bootstrap router.

--- client/src/index.js
+++ client/src/index.js
@@ -1,15 +1,30 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import {
+    Route,
+    RouterProvider,
+    createBrowserRouter,
+    createRoutesFromElements,
+} from 'react-router-dom'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './index.css'
 import App from './App'
 import reportWebVitals from './reportWebVitals'
+import HomeScreen from './screens/HomeScreen'
+
+const router = createBrowserRouter(
+    createRoutesFromElements(
+        <Route path="/" element={<App />}>
+            <Route index path="/" element={<HomeScreen />} />
+        </Route>
+    )
+)
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <React.StrictMode>
+        <RouterProvider router={router} />
+    </React.StrictMode>
 )
 
 // If you want to start measuring performance in your app, pass a function
