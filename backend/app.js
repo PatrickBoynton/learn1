c7413255beb88641a67974680b7cd8404e5e786f
PatrickBoynton <jpboynton3@gmail.com>
2023-06-18 20:37:14 -0400

    Added the product routes.

--- backend/app.js
+++ backend/app.js
@@ -1,4 +1,5 @@
 import express from 'express'
+import products from './data/products.js'
 
 const port = 8000 || process.env.PORT
 
@@ -6,6 +7,15 @@ const app = express()
 
 app.get('/', (req, res, next) => {
     res.send('API IS RUNNING')
+})
+
+app.get('/api/product', (req, res, next) => {
+    res.json(products)
+})
+
+app.get('/api/product/:id', (req, res, next) => {
+    const product = products.find((p) => p._id === req.params.id)
+    res.json(product)
 })
 
 app.listen(port, () => console.log(`Go to http://localhost:${port}`))
