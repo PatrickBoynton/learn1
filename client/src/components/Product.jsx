4877f480c88460acd7de6a72633549098af8c40a
PatrickBoynton <jpboynton3@gmail.com>
2023-06-19 13:00:48 -0400

    Changed id to _id.

--- client/src/components/Product.jsx
+++ client/src/components/Product.jsx
@@ -2,14 +2,15 @@ import React from 'react'
 import { Card } from 'react-bootstrap'
 import { Link } from 'react-router-dom'
 import Rating from '../components/Rating'
+
 const Product = ({ product }) => {
     return (
         <Card className="my-3 p-3 rounded">
-            <Link to={`/product/${product.id}`}>
+            <Link to={`/product/${product._id}`}>
                 <Card.Img src={product.image} variant="top" />
             </Link>
             <Card.Body>
-                <Link to={`/product/${product.id}`}>
+                <Link to={`/product/${product._id}`}>
                     <Card.Title as="div">
                         <strong>{product.name}</strong>
                     </Card.Title>
