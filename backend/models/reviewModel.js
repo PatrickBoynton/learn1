5b3f627309067a2b8d2c61b735676b948ce9dfa8
PatrickBoynton <jpboynton3@gmail.com>
2023-06-20 11:38:57 -0400

    Added the initial models.

---
+++ backend/models/reviewModel.js
@@ -0,0 +1,30 @@
+import mongoose from 'mongoose'
+
+const reviewSchema = new mongoose.Schema(
+    {
+        user: {
+            type: mongoose.Schema.Types.ObjectId,
+            requied: true,
+            ref: 'User',
+        },
+        name: {
+            type: String,
+            required: true,
+        },
+        rating: {
+            type: Number,
+            required: true,
+        },
+        comment: {
+            type: String,
+            required: true,
+        },
+    },
+    {
+        timestamps: true,
+    }
+)
+
+const Review = mongoose.model('Review', reviewSchema)
+
+export default Review
