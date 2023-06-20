31b8ad716edc9cd630f127c1e93ca8ff38d45e77
PatrickBoynton <jpboynton3@gmail.com>
2023-06-20 11:55:34 -0400

    Created users.

---
+++ backend/data/users.js
@@ -0,0 +1,24 @@
+import bcrypt from 'bcryptjs'
+
+const users = [
+    {
+        name: 'Admin1',
+        email: 'admin@gametronics.com',
+        password: bcrypt.hashSync('safepass1', 10),
+        isAdmin: true,
+    },
+    {
+        name: 'JPBIII',
+        email: 'jpb3@email.com',
+        password: bcrypt.hashSync('safepass1', 10),
+        isAdmin: false,
+    },
+    {
+        name: 'tyrion101',
+        email: 'tyrion101@email.com',
+        password: bcrypt.hashSync('safepass1', 10),
+        isAdmin: false,
+    },
+]
+
+export default users
