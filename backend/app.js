c77f6a637efc1d67a2ca86c3bbf009aba1a555ff
PatrickBoynton <jpboynton3@gmail.com>
2023-06-20 18:26:11 -0400

    Created middleware to handle errors.

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import productRoutes from './routes/productRoutes.js'
 import dotenv from 'dotenv'
+import { notFound, errorHandler } from './middleware/errorMiddleware.js'
 dotenv.config()
 import { connectDB } from './config/db.js'
 
@@ -11,9 +12,13 @@ connectDB()
 const app = express()
 
 app.get('/', (req, res, next) => {
-    res.send('API IS RUNNING')
+  res.send('API IS RUNNING')
 })
 
 app.use('/api/product/', productRoutes)
+
+app.use(notFound)
+
+app.use(errorHandler)
 
 app.listen(port, () => console.log(`Go to http://localhost:${port}`))
