7c179bf43b62b36a92d1b1bb251966a6880f79c5
PatrickBoynton <jpboynton3@gmail.com>
2023-06-21 18:16:26 -0400

    Changed the base url.

--- client/src/constants.js
+++ client/src/constants.js
@@ -1,6 +1,4 @@
-export const BASE_URL =
-  process.env.NODE_ENV === 'development' ? 'http://localhost:8000' : ''
-
+export const BASE_URL = ''
 export const PRODUCTS_URL = '/api/product'
 export const ORDERS_URL = '/api/order'
 export const PAYPAL_URL = '/api/config/paypal'
