56eefb0617fc2454fced44d9a88d35f680d81958
PatrickBoynton <jpboynton3@gmail.com>
2023-06-21 18:16:38 -0400

    Used the new product slice.

--- client/src/screens/HomeScreen.jsx
+++ client/src/screens/HomeScreen.jsx
@@ -1,31 +1,29 @@
 import { Row, Col } from 'react-bootstrap'
 import Product from '../components/Product'
-import axios from 'axios'
-import { useEffect, useState } from 'react'
+import { useGetProductsQuery } from '../slices/productApiSlice'
 
 const HomeScreen = () => {
-    const [products, setProducts] = useState([])
-
-    useEffect(() => {
-        const fetchProducts = async () => {
-            const { data } = await axios.get('/api/product/')
-            setProducts(data)
-        }
-        fetchProducts()
-    }, [])
-
-    return (
+  const { data: products, isLoading, isError } = useGetProductsQuery()
+  return (
+    <>
+      {isLoading ? (
+        <h1>Loading...</h1>
+      ) : isError ? (
+        <p>{isError?.data?.message || isError?.error}</p>
+      ) : (
         <>
-            <h1>Latest Products</h1>
-            <Row>
-                {products?.map((product) => (
-                    <Col key={product?._id} sm={12} md={6} lg={4} xl={3}>
-                        <Product product={product} />
-                    </Col>
-                ))}
-            </Row>
+          <h1>Latest Products</h1>
+          <Row>
+            {products?.map(product => (
+              <Col key={product?._id} sm={12} md={6} lg={4} xl={3}>
+                <Product product={product} />
+              </Col>
+            ))}
+          </Row>
         </>
-    )
+      )}
+    </>
+  )
 }
 
 export default HomeScreen
