e6afa5dcd5b9afb1db21bc5f26877235e150b9fd
PatrickBoynton <jpboynton3@gmail.com>
2023-06-21 14:44:56 -0400

    Set up the redux store.

--- client/src/index.js
+++ client/src/index.js
@@ -1,10 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import {
-    Route,
-    RouterProvider,
-    createBrowserRouter,
-    createRoutesFromElements,
+  Route,
+  RouterProvider,
+  createBrowserRouter,
+  createRoutesFromElements,
 } from 'react-router-dom'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './index.css'
@@ -12,21 +12,25 @@ import App from './App'
 import reportWebVitals from './reportWebVitals'
 import HomeScreen from './screens/HomeScreen'
 import ProductScreen from './screens/ProductScreen'
+import { Provider } from 'react-redux'
+import store from './store'
 
 const router = createBrowserRouter(
-    createRoutesFromElements(
-        <Route path="/" element={<App />}>
-            <Route index path="/" element={<HomeScreen />} />
-            <Route path="/product/:productId" element={<ProductScreen />} />
-        </Route>
-    )
+  createRoutesFromElements(
+    <Route path='/' element={<App />}>
+      <Route index path='/' element={<HomeScreen />} />
+      <Route path='/product/:productId' element={<ProductScreen />} />
+    </Route>
+  )
 )
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
-    <React.StrictMode>
-        <RouterProvider router={router} />
-    </React.StrictMode>
+  <React.StrictMode>
+    <Provider store={store}>
+      <RouterProvider router={router} />
+    </Provider>
+  </React.StrictMode>
 )
 
 // If you want to start measuring performance in your app, pass a function
