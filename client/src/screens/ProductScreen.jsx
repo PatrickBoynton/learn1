f4de750510cd84666aabb2df11c6da9111c9bab5
PatrickBoynton <jpboynton3@gmail.com>
2023-06-22 21:54:01 -0400

    Added the ability to add a qty, and the product to localStorage

--- client/src/screens/ProductScreen.jsx
+++ client/src/screens/ProductScreen.jsx
@@ -1,31 +1,42 @@
-import { Link, useParams } from 'react-router-dom'
-import { Button, Card, Col, Image, ListGroup, Row } from 'react-bootstrap'
+import { Link, useParams, useNavigate } from 'react-router-dom'
+import { Button, Card, Col, Image, ListGroup, Row, Form } from 'react-bootstrap'
 import Rating from '../components/Rating'
 import { useGetProductQuery } from '../slices/productApiSlice.js'
 import Message from '../components/Message'
 import Loader from '../components/Loader'
+import { useState } from 'react'
+import { addToCart } from '../slices/cartSlice.js'
+import { useDispatch } from 'react-redux'
 
 const ProductScreen = () => {
+  const [qty, setQty] = useState(1)
   const { productId } = useParams()
   const { data: product, isLoading, error } = useGetProductQuery(productId)
+  const count = [...Array(product?.countInStock).keys()]
+  const dispatch = useDispatch()
+  const navigate = useNavigate()
 
+  const addToCartHandler = () => {
+    dispatch(addToCart({ ...product, qty }))
+    navigate('/cart')
+  }
   return (
     <>
-      <Link className='btn btn-light my-3' to='/'>
+      <Link className="btn btn-light my-3" to="/">
         Go Back
       </Link>
 
       {isLoading ? (
         <Loader />
       ) : error ? (
-        <Message variant='danger'>{error.data.message || error.error}</Message>
+        <Message variant="danger">{error.data.message || error.error}</Message>
       ) : (
         <Row>
           <Col md={5}>
             <Image src={product.image} alt={product.name} fluid />
           </Col>
           <Col md={4}>
-            <ListGroup variant='flush'>
+            <ListGroup variant="flush">
               <ListGroup.Item>
                 <h3>{product.name}</h3>
               </ListGroup.Item>
@@ -43,7 +54,7 @@ const ProductScreen = () => {
           </Col>
           <Col md={3}>
             <Card>
-              <ListGroup variant='flush'>
+              <ListGroup variant="flush">
                 <ListGroup.Item>
                   <Row>
                     <Col>Price: </Col>
@@ -54,15 +65,35 @@ const ProductScreen = () => {
                   <Row>
                     <Col>Status: </Col>
                     <Col>
-                      {product.countInStock > 0 ? 'In stock' : 'Out of stock'}
+                      {product?.countInStock > 0 ? 'In stock' : 'Out of stock'}
                     </Col>
                   </Row>
                 </ListGroup.Item>
+                {product?.countInStock > 0 && (
+                  <ListGroup.Item>
+                    <Row>
+                      <Col>Qty</Col>
+                      <Col>
+                        <Form.Control
+                          as="select"
+                          value={qty}
+                          onChange={e => setQty(Number(e.target.value))}>
+                          {count.map(x => (
+                            <option key={x + 1} value={x + 1}>
+                              {x + 1}
+                            </option>
+                          ))}
+                        </Form.Control>
+                      </Col>
+                    </Row>
+                  </ListGroup.Item>
+                )}
                 <ListGroup.Item>
                   <Button
-                    className='btn-block'
-                    type='button'
-                    disabled={product.countInStock === 0}>
+                    className="btn-block"
+                    type="button"
+                    onClick={addToCartHandler}
+                    disabled={product?.countInStock === 0}>
                     Add to Cart
                   </Button>
                 </ListGroup.Item>
