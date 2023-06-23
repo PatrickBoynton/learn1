eae3d9ad88fd6ac8f709810945b10c70bac65346
PatrickBoynton <jpboynton3@gmail.com>
2023-06-22 22:13:09 -0400

    Created a cartUtils file then moved some of the logic here.

---
+++ client/src/utils/cartUtils.js
@@ -0,0 +1,23 @@
+export const addDecimals = num => (Math.round(num * 100) / 100).toFixed(2)
+
+export const updateCart = state => {
+  // item price
+  state.itemPrice = addDecimals(
+    state.cartItems.reduce((acc, item) => acc + item.price * item.qty, 0)
+  )
+
+  // Shipping price
+  state.shippingPrice = addDecimals(state.itemsPrice > 100 ? 0 : 10)
+
+  // Tax price (6%)
+  state.taxPrice = addDecimals(Number(0.06 * state.itemsPrice))
+
+  // Total price
+  state.totalPrice = addDecimals(
+    Number(state.itemPrice + state.shippingPrice + state.taxPrice)
+  )
+
+  localStorage.setItem('cart', state)
+
+  return state
+}
