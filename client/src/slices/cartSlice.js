cfb03e615e178b5b69701aea9ccf688880f77a83
PatrickBoynton <jpboynton3@gmail.com>
2023-06-22 22:13:24 -0400

    Used the updateCart function.

--- client/src/slices/cartSlice.js
+++ client/src/slices/cartSlice.js
@@ -1,10 +1,9 @@
 import { createSlice } from '@reduxjs/toolkit'
+import { updateCart } from '../utils/cartUtils'
 
 const cart = localStorage.getItem('cart')
 
 const initialState = cart ? JSON.parse(cart) : { cartItems: [] }
-
-const addDecimals = num => (Math.round(num * 100) / 100).toFixed(2)
 
 const cartSlice = createSlice({
   name: 'cart',
@@ -22,23 +21,7 @@ const cartSlice = createSlice({
         state.cartItems = [...state.cartItems, item]
       }
 
-      // item price
-      state.itemPrice = addDecimals(
-        state.cartItems.reduce((acc, item) => acc + item.price * item.qty, 0)
-      )
-
-      // Shipping price
-      state.shippingPrice = addDecimals(state.itemsPrice > 100 ? 0 : 10)
-
-      // Tax price (6%)
-      state.taxPrice = addDecimals(Number(0.06 * state.itemsPrice))
-
-      // Total price
-      state.totalPrice = addDecimals(
-        Number(state.itemPrice + state.shippingPrice + state.taxPrice)
-      )
-
-      localStorage.setItem('cart', state)
+      updateCart(state)
     },
   },
 })
