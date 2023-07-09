e97dc00b0cee894a8395b9e0f7faaf2ab7d2c7b6
Patrick Boynton <jpboynton3@gmail.com>
2023-07-09 16:08:06 -0400

    Added encoding and json.

--- backend/app.js
+++ backend/app.js
@@ -12,6 +12,9 @@ connectDB()
 
 const app = express()
 
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
 app.get('/', (req, res, next) => {
   res.send('API IS RUNNING')
 })
