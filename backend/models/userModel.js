2846beef4ed38b076594e0ca9f8af3ef14ec332d
Patrick Boynton <jpboynton3@gmail.com>
2023-07-09 16:08:53 -0400

    Created a method to compare passswords.

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,30 +1,33 @@
 import mongoose from 'mongoose'
 
 const userSchema = new mongoose.Schema(
-    {
-        name: {
-            type: String,
-            required: true,
-        },
-        email: {
-            type: String,
-            required: true,
-            unique: true,
-        },
-        password: {
-            type: String,
-            required: true,
-        },
-        isAdmin: {
-            type: Boolean,
-            required: true,
-            default: false,
-        },
+  {
+    name: {
+      type: String,
+      required: true,
     },
-    {
-        timestamps: true,
-    }
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    isAdmin: {
+      type: Boolean,
+      required: true,
+      default: false,
+    },
+  },
+  {
+    timestamps: true,
+  }
 )
+
+userSchema.methods.matchPassword = async password =>
+  await bcrypt.compare(password, this.password)
 
 const User = mongoose.model('User', userSchema)
 
