687a4aa9ca712bdc5d2f2eab3aa77ea681329603
Patrick Boynton <jpboynton3@gmail.com>
2023-07-09 14:34:12 -0400

    added a dotenv variable.

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,11 +1,14 @@
 import mongoose from 'mongoose'
+import dotenv from 'dotenv'
+
+dotenv.config({ path: '../.env' })
 
 export const connectDB = async () => {
-    try {
-        const conn = await mongoose.connect(process.env.URI)
-        console.log(`MongoDB connected to ${conn.connection.host}`)
-    } catch (error) {
-        console.error(`ERROR: ${error.message}`)
-        process.exit(1)
-    }
+  try {
+    const conn = await mongoose.connect(process.env.URI)
+    console.log(`MongoDB connected to ${conn.connection.host}`)
+  } catch (error) {
+    console.error(`ERROR: ${error.message}`)
+    process.exit(1)
+  }
 }
