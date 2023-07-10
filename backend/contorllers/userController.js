50ee5bf35f38fa16542c4a9e6e5e17498e5f6ddc
Patrick Boynton <jpboynton3@gmail.com>
2023-07-10 09:21:25 -0400

    Added registration logic.

--- backend/contorllers/userController.js
+++ backend/contorllers/userController.js
@@ -1,9 +1,30 @@
 import { asyncHandler } from '../middleware/asyncHandler.js'
 import User from '../models/userModel.js'
 import jwt from 'jsonwebtoken'
+import { generateToken } from '../utils/generateToken.js'
 
 export const registerUser = asyncHandler(async (req, res, next) => {
-  res.send('register user')
+  const { name, email, password } = req.body
+  const userExists = await User.findOne({ email })
+  if (userExists) {
+    res.status(400)
+    throw new Error('User already exists!')
+  }
+
+  const user = await User.create({ name, email, password })
+
+  if (user) {
+    generateToken(res, user._id)
+
+    res.status(201).json({
+      _id: user._id,
+      email: user.email,
+      password: user.password,
+    })
+  } else {
+    res.status(400)
+    throw new Error('Invalid user data. ')
+  }
 })
 
 export const logoutUser = asyncHandler(async (req, res, next) => {
@@ -20,17 +41,8 @@ export const loginUser = asyncHandler(async (req, res, next) => {
   const user = await User.findOne({ email })
 
   if (user && user.matchPassword(password)) {
-    const token = jwt.sign({ userId: user._id }, process.env.JWT_SERCRET, {
-      expiresIn: '365d',
-    })
+    generateToken(res, user._id)
 
-    res.cookie('jwt', token, {
-      httpOnly: true,
-      secure: process.env.NODE_ENV !== 'development',
-      sameSite: 'strict',
-      //  365 days in milliseconds
-      maxAge: 31536000000,
-    })
     res.json({
       _id: user._id,
       name: user.name,
