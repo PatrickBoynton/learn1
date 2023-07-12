c680d003e7d77a9783ba1b1c85f5b1fb02468a49
Patrick Boynton <jpboynton3@gmail.com>
2023-07-12 09:15:32 -0400

    Changed the url.

--- client/src/slices/usersApiSlice.js
+++ client/src/slices/usersApiSlice.js
@@ -1,10 +1,11 @@
 import { apiSlice } from './apiSlice'
+import { USERS_URL } from '../constants'
 
 export const usersApiSlice = apiSlice.injectEndpoints({
   endpoints: builder => ({
     login: builder.mutation({
       query: data => ({
-        url: USERS_URL / auth,
+        url: `${USERS_URL}/login`,
         method: 'POST',
         body: data,
       }),
