8dd00f5b2af22720198cb6208bbbcffa578fd523
Patrick Boynton <jpboynton3@gmail.com>
2023-07-14 10:07:30 -0400

    Added the new routes.

---
+++ backend/routes/orderRoutes.js
@@ -0,0 +1,21 @@
+import express from 'express'
+import {
+  addOrderItems,
+  getAllOrders,
+  getMyOrders,
+  getOrderById,
+  updateOrderToDelivered,
+  updateOrderToPaid,
+} from '../contorllers/orderController.js'
+import { protect, admin } from '../middleware/authMiddleware.js'
+
+const router = express.Router()
+
+router.post('/', protect, addOrderItems)
+router.get('/', protect, admin, getAllOrders)
+router.get('/mine', protect, getMyOrders)
+router.get('/:id', protect, admin, getOrderById)
+router.put('/:id/pay', protect, admin, updateOrderToPaid)
+router.put('/:id/deliver', protect, admin, updateOrderToDelivered)
+
+export default router
