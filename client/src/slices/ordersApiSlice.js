4181bf11c6f1ad7418e57fdedb16354682943ec2
Patrick Boynton <jpboynton3@gmail.com>
2023-07-18 08:43:47 -0400

    Added a slice to get the details for an order.

--- client/src/slices/ordersApiSlice.js
+++ client/src/slices/ordersApiSlice.js
@@ -10,7 +10,13 @@ export const ordersApiSlice = apiSlice.injectEndpoints({
         body: { ...order },
       }),
     }),
+    getOrderDetails: builder.query({
+      query: id => ({
+        url: `${ORDERS_URL}/${id}`,
+      }),
+    }),
   }),
 })
 
-export const { useCreateOrderMutation } = ordersApiSlice
+export const { useCreateOrderMutation, useGetOrderDetailsQuery } =
+  ordersApiSlice
