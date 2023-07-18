eec9b30577b503443c31f6f14d3fafd715f7d94e
Patrick Boynton <jpboynton3@gmail.com>
2023-07-18 09:00:01 -0400

    Added the logic to the updateOrder enpoint.

--- backend/contorllers/orderController.js
+++ backend/contorllers/orderController.js
@@ -1,5 +1,6 @@
 import { asyncHandler } from '../middleware/asyncHandler.js'
 import Order from '../models/orderModel.js'
+import { errorCondition } from '../utils/errorCondition.js'
 
 export const addOrderItems = asyncHandler(async (req, res) => {
   const {
@@ -57,7 +58,23 @@ export const getOrderById = asyncHandler(async (req, res) => {
 })
 
 export const updateOrderToPaid = asyncHandler(async (req, res) => {
-  res.send('Update the order status.')
+  const order = await Order.findById(req.params.id)
+
+  if (order) {
+    order.isPaid = true
+    order.paidAt = Date.now()
+    order.paymentResult = {
+      id: req.body.id,
+      status: req.body.status,
+      update_time: req.body.update_time,
+      email_address: req.body.email_address,
+    }
+    const updatedOrder = await order.save()
+
+    res.status(200).json(updatedOrder)
+  } else {
+    errorCondition(res, 404, 'Error not found.')
+  }
 })
 export const updateOrderToDelivered = asyncHandler(async (req, res) => {
   res.send('Update the order status.')
