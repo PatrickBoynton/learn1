fbf02b2858401cf498f440104eda4cdb34d7fa5a
Patrick Boynton <jpboynton3@gmail.com>
2023-07-18 09:00:14 -0400

    Removed admin.

--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -12,7 +12,7 @@ import { protect, admin } from '../middleware/authMiddleware.js'
 const router = express.Router()
 
 router.post('/', protect, addOrderItems)
-router.get('/', protect, admin, getAllOrders)
+router.get('/', protect, getAllOrders)
 router.get('/mine', protect, getMyOrders)
 router.get('/:id', protect, admin, getOrderById)
 router.put('/:id/pay', protect, admin, updateOrderToPaid)
