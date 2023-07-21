486f964bc20dc9db585431b72414dc68d959c739
Patrick Boynton <jpboynton3@gmail.com>
2023-07-21 09:08:44 -0400

    Created the screen to display them on.

--- client/src/screens/admin/OrderListScreen.jsx
+++ client/src/screens/admin/OrderListScreen.jsx
@@ -1,5 +1,67 @@
+import { useGetAllOrdersQuery } from '../../slices/ordersApiSlice'
+import Loader from '../../components/Loader'
+import Message from '../../components/Message'
+import { Button, Table } from 'react-bootstrap'
+import { FaTimes } from 'react-icons/fa'
+import { LinkContainer } from 'react-router-bootstrap'
+
 const OrderListScreen = () => {
-  return <></>
+  const { data: orders, isLoading, error } = useGetAllOrdersQuery()
+  return (
+    <>
+      <h1>Orders</h1>
+      {isLoading ? (
+        <Loader />
+      ) : error ? (
+        <Message variant="danger">error</Message>
+      ) : (
+        <Table striped border hover responsive className="table-sm">
+          <thead>
+            <tr>
+              <th>Id</th>
+              <th>User</th>
+              <th>Date</th>
+              <th>Total</th>
+              <th>Paid</th>
+              <th>Delivered</th>
+              <th></th>
+            </tr>
+          </thead>
+          <tbody>
+            {orders.map(order => (
+              <tr key={order._id}>
+                <td>{order._id}</td>
+                <td>{order.user && order.user.name}</td>
+                <td>{order.createdAt}</td>
+                <td>{order.totalPrice}</td>
+                <td>
+                  {order.isPaid ? (
+                    order.paidAt.substring(0, 10)
+                  ) : (
+                    <FaTimes style={{ color: 'red' }} />
+                  )}
+                </td>
+                <td>
+                  {order.isDelivered ? (
+                    order.deliveredAt.substring(0, 10)
+                  ) : (
+                    <FaTimes style={{ color: 'red' }} />
+                  )}
+                </td>
+                <td>
+                  <LinkContainer to={`/order/${order._id}`}>
+                    <Button variant="light" className="btn-sm">
+                      Details
+                    </Button>
+                  </LinkContainer>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </Table>
+      )}
+    </>
+  )
 }
 
 export default OrderListScreen
