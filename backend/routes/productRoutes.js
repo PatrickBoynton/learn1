0cb4812b8fc2f259fbabb5792bbec03d0643b818
Patrick Boynton <jpboynton3@gmail.com>
2023-07-25 08:46:57 -0400

    Added the route for the createProduct controller.

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,14 +1,17 @@
 import express from 'express'
-
 import {
   getProductById,
   getProducts,
+  createProduct,
 } from '../contorllers/productController.js'
+import { admin, protect } from '../middleware/authMiddleware.js'
 
 const router = express.Router()
 
 router.get('/', getProducts)
 
 router.get('/:id', getProductById)
+
+router.post('/', protect, admin, createProduct)
 
 export default router
