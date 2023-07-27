bd9144f6a921c2452a7fd65844a9ffcfde416883
Patrick Boynton <jpboynton3@gmail.com>
2023-07-27 09:56:51 -0400

    Added the upload route, and the static path for the uploads.

--- backend/app.js
+++ backend/app.js
@@ -7,6 +7,8 @@ import { connectDB } from './config/db.js'
 import userRoutes from './routes/userRoutes.js'
 import cookieParser from 'cookie-parser'
 import orderRoutes from './routes/orderRoutes.js'
+import uploadRoutes from './routes/uploadRoutes.js'
+import path from 'path'
 
 const port = process.env.PORT || Math.round(Math.random() * (5000 - 8000))
 
@@ -25,10 +27,15 @@ app.get('/', (req, res) => {
 app.use('/api/product/', productRoutes)
 app.use('/api/user/', userRoutes)
 app.use('/api/order/', orderRoutes)
+app.use('/api/upload/', uploadRoutes)
 
 app.get('/api/config/paypal', (req, res) =>
   res.send({ clientId: process.env.PAYPAL_CLIENT_ID })
 )
+
+const __dirname = path.resolve()
+
+app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
 
 app.use(notFound)
 
