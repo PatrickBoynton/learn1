1fe6e7141f1f3d138e1202d61db1dc0760cc346d
Patrick Boynton <jpboynton3@gmail.com>
2023-08-15 13:09:34 -0400

    Created a component for changing the metadata.

---
+++ client/src/components/Meta.jsx
@@ -0,0 +1,19 @@
+import { Helmet } from 'react-helmet-async'
+
+const Meta = ({ title, description, keywords }) => {
+  return (
+    <Helmet>
+      <title>{title}</title>
+      <meta name="description" content={description} />
+      <meta name="keywords" content={keywords} />
+    </Helmet>
+  )
+}
+
+Meta.defaultProps = {
+  title: 'Gametronics',
+  description: 'Quality gaming and electronics',
+  keywords: 'electronics, games, videogames, quality',
+}
+
+export default Meta
