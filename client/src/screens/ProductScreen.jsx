f2793f7548c0d7dd41dfdde7d36eacf4f1290524
Patrick Boynton <jpboynton3@gmail.com>
2023-08-15 13:10:47 -0400

    Added helmet to the app.

--- client/src/screens/ProductScreen.jsx
+++ client/src/screens/ProductScreen.jsx
@@ -20,6 +20,7 @@ import { useState } from 'react'
 import { addToCart } from '../slices/cartSlice.js'
 import { useDispatch, useSelector } from 'react-redux'
 import { toast } from 'react-toastify'
+import Meta from '../components/Meta'
 
 const ProductScreen = () => {
   const [qty, setQty] = useState(1)
@@ -73,6 +74,7 @@ const ProductScreen = () => {
         <Message variant="danger">{error.data.message || error.error}</Message>
       ) : (
         <>
+          <Meta title={product.name} description={product.description} />
           <Row>
             <Col md={5}>
               <Image src={product.image} alt={product.name} fluid />
