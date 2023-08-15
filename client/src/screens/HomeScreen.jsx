89a476210a0cc47a1e9b0afd3c80a68e1d95ba24
Patrick Boynton <jpboynton3@gmail.com>
2023-08-15 08:52:32 -0400

    Added keyword to the params.

--- client/src/screens/HomeScreen.jsx
+++ client/src/screens/HomeScreen.jsx
@@ -7,8 +7,11 @@ import { useParams } from 'react-router-dom'
 import Paginate from '../components/Paginate'
 
 const HomeScreen = () => {
-  const { pageNumber } = useParams()
-  const { data, isLoading, error } = useGetProductsQuery({ pageNumber })
+  const { pageNumber, keyword } = useParams()
+  const { data, isLoading, error } = useGetProductsQuery({
+    pageNumber,
+    keyword,
+  })
   return (
     <>
       {isLoading ? (
