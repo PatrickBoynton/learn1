39dbc8bc8409df631df9eb40bb13c4cc19cc12d5
Patrick Boynton <jpboynton3@gmail.com>
2023-10-16 09:45:41 -0400

    Updated the type of array.

--- backend/data/users.ts
+++ backend/data/users.ts
@@ -1,24 +1,25 @@
 import bcrypt from 'bcryptjs'
+import { IUser } from '../interfaces/modelInterfaces'
 
-const users = [
-    {
-        name: 'Admin1',
-        email: 'admin@gametronics.com',
-        password: bcrypt.hashSync('safepass1', 10),
-        isAdmin: true,
-    },
-    {
-        name: 'JPBIII',
-        email: 'jpb3@email.com',
-        password: bcrypt.hashSync('safepass1', 10),
-        isAdmin: false,
-    },
-    {
-        name: 'tyrion101',
-        email: 'tyrion101@email.com',
-        password: bcrypt.hashSync('safepass1', 10),
-        isAdmin: false,
-    },
+const users: IUser[] = [
+  {
+    name: 'Admin1',
+    email: 'admin@gametronics.com',
+    password: bcrypt.hashSync('safepass1', 10),
+    isAdmin: true,
+  },
+  {
+    name: 'JPBIII',
+    email: 'jpb3@email.com',
+    password: bcrypt.hashSync('safepass1', 10),
+    isAdmin: false,
+  },
+  {
+    name: 'tyrion101',
+    email: 'tyrion101@email.com',
+    password: bcrypt.hashSync('safepass1', 10),
+    isAdmin: false,
+  },
 ]
 
 export default users
